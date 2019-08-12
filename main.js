58352adb5840acc5e1b00a0fcd216b256c179dd9
Rashell Marcelino <rashellmarcelino@rashells-air.home>
2019-08-12 10:47:27 -0400

    created js funtions

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 console.log("rashell did it");
+let choicesArray = [];
+let arraySize;
+let randchoice;
+let choice;
+// console.log(choice);
+
+let userInput;
+
+// gets user input
+function createList() {
+  for (let i = 0; i < 4; i++) {
+    userInput = window.prompt("what you want to watch");
+    choicesArray.push(userInput);
+  }
+  console.log(choicesArray);
+  return choicesArray;
+}
+
+function decision(array) {
+  arraySize = array.length;
+  randchoice = Math.floor(Math.random() * arraySize);
+
+  choice = array[randchoice];
+  console.log("choice " + choice);
+  console.log("random num " + randchoice);
+  console.log("array size " + arraySize);
+}
+
+let makelist = createList();
+decision(makelist);
