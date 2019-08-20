daee5520677a0854dcf529b930bf269b992fed4a
Rashell Marcelino <rashellmarcelino@rashells-air.home>
2019-08-20 11:45:28 -0400

    delete button - not functional yet

--- main.js
+++ main.js
@@ -3,16 +3,27 @@ let addBtnElement = document.getElementById("add-btn");
 let inputValue = document.getElementById("option-input");
 let choicesArray = [];
 
+function addOption() {
+  let deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("deleteBtn");
+  deleteBtn.innerHTML = "DELETE";
+
+  let item = inputValue.value;
+  let text = document.createTextNode(item);
+  let newitem = document.createElement("li");
+  newitem.appendChild(text);
+
+  document.getElementById("allOptions").appendChild(newitem);
+  document.getElementById("allOptions").appendChild(deleteBtn);
+}
+
+// function deleteList() {
+//   for (let i = 0; i<optionList.length;i++){
+
+//   }
+// }
 addBtnElement.addEventListener("click", function() {
   choicesArray.push(inputValue.value);
   console.log(choicesArray);
   inputValue.value = "";
 });
-
-function addOption() {
-  let item = inputValue.value;
-  let text = document.createTextNode(item);
-  let newitem = document.createElement("li");
-  newitem.appendChild(text);
-  document.getElementById("allOptions").appendChild(newitem);
-}
